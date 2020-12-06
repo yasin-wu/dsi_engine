807c8979078069c4ddceac764a2611a531300c86
yasin <wuyang@wondersoft.cn>
2020-12-06 11:04:26 +0800

    update gohs_test.go

--- gohs_test.go
+++ gohs_test.go
@@ -1,16 +1,21 @@
 package dlp
 
 import (
-	"github.com/flier/gohs/hyperscan"
 	"testing"
+
+	"github.com/flier/gohs/hyperscan"
 )
 
 func TestGohs(t *testing.T) {
-	inputData := "测试IPV6地址正则:fe80::ec61:c1d1:9827:82be%13"
+	inputData := "My name is Bob;电话号码:18108279331;测试IPV6地址正则:fe80::ec61:c1d1:9827:82be%13"
 	var patterns []*hyperscan.Pattern
-	pattern := hyperscan.NewPattern(IPV6Reg, hyperscan.SomLeftMost|hyperscan.Utf8Mode)
-	pattern.Id = 1
-	patterns = append(patterns, pattern)
+	pattern1 := hyperscan.NewPattern(IPV6Reg, hyperscan.SomLeftMost|hyperscan.Utf8Mode)
+	pattern1.Id = 1
+	pattern2 := hyperscan.NewPattern("Bob", hyperscan.SomLeftMost|hyperscan.Utf8Mode)
+	pattern2.Id = 2
+	pattern3 := hyperscan.NewPattern(TelNumReg, hyperscan.SomLeftMost|hyperscan.Utf8Mode)
+	pattern3.Id = 3
+	patterns = append(patterns, pattern1, pattern2, pattern3)
 	gohs := &Gohs{
 		Patterns: patterns,
 	}
