ee382546c62e0235f5f68c5747cee52f7ab0dfa3
yasin <yasin_wu@qq.com>
2021-11-15 09:15:56 +0800

    update:修改部分敏感信息

--- test/regexp_engine_test.go
+++ test/regexp_engine_test.go
@@ -21,9 +21,9 @@ func TestRegexpEngine(t *testing.T) {
 	}
 	rulesMap := rule.RulesMap
 	inputData := "My name is Bob;" +
-		"电话号码:18108279331;" +
+		"电话号码:18108379230;" +
 		"测试IPV6地址正则:fe80::ec61:c1d1:9827:82be%13;" +
-		"地址信息:四川省成都市武侯区府城大道天府新谷6号楼607室."
+		"地址信息:四川省成都市武侯区府城大道天府新谷8号楼1007室."
 	regexp1 := &regexp_engine.Regexp{
 		Id:     1,
 		Regexp: rulesMap["IPV6"].(map[string]interface{})["rule"].(string),
